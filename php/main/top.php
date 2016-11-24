87d6b4c60a09841b931dca44bfb0adc4b576ed15
Iida Taiki <rild3178ata@gmail.com>
2016-11-25 08:01:14 +0900

    start to top

--- php/main/top.php
+++ php/main/top.php
@@ -1,17 +1,92 @@
 <html>
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>イベント管理サイト(仮)</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Keio Piano Society</title>
+
+  <link rel="stylesheet" type="text/css" href="../../css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="../../assets/css/rild.css">
+
+  <style type="text/css">
+
+  </style>
+
+  <!--[if lt IE 9]>
+    <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
 </head>
 
 <!-- like index.php -->
 <body>
 
-  <h1>メニュー</h1>
-  <a href="search.php">イベント検索</a><br>
-  <a href="list.php">イベント一覧</a><br>
-  <a href="create.php">イベント作成</a><br>
-  <a href="password.php">パスワード変更</a><br>
-  <a href="logout.php">ログアウト</a><br>
+
+  <header>
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="../../" class="navbar-brand">KPS</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Top</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Menu<span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Kousaka Honoka</a></li>
+                <li><a href="#">Minami Kotori</a></li>
+                <li><a href="#">Sonoda Umi</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Koizumi Hanayo</a></li>
+                <li><a href="#">Hoshizora Rin</a></li>
+                <li><a href="#">Nishikino Maki</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Yazawa Nico</a></li>
+                <li><a href="#">Ayase Eli</a></li>
+                <li><a href="#">Tojo Nozomi</a></li>
+              </ul>
+            </li>
+            <li><a href="//github.com/windyakin/Honoka/releases">User</a></li>
+          </ul>
+          <!-- search -->
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../../php/users/signin/login.php">Logout</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <section class="section section-default">
+  <div class="container">
+    <h1>メニュー</h1>
+    <a href="search.php">イベント検索</a><br>
+    <a href="list.php">イベント一覧</a><br>
+    <a href="create.php">イベント作成</a><br>
+    <a href="password.php">パスワード変更</a><br>
+    <a href="logout.php">ログアウト</a><br>
+  </div>
+  </section>
+
+  <footer class="small">
+  </footer>
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="../../js/bootstrap.min.js"></script>
+
 </body>
 </html>
