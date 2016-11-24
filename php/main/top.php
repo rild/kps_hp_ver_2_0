f65a88bcd626b9f1fb097f264ca5d6d43a180fb8
Iida Taiki <rild3178ata@gmail.com>
2016-11-24 13:08:48 +0900

    add main

---
+++ php/main/top.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>イベント管理サイト(仮)</title>
+</head>
+
+<!-- like index.php -->
+<body>
+
+  <h1>メニュー</h1>
+  <a href="search.php">イベント検索</a><br>
+  <a href="list.php">イベント一覧</a><br>
+  <a href="create.php">イベント作成</a><br>
+  <a href="password.php">パスワード変更</a><br>
+  <a href="logout.php">ログアウト</a><br>
+</body>
+</html>
